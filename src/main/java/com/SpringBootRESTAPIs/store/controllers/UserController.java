d4c0f8900f67fb8365670e6d5100513b7fdd9332
Arash Karampour <karampour.arash@yahoo.com>
2026-02-15 00:40:57 +0100

    getting all a user using pathvariable and id

--- src/main/java/com/SpringBootRESTAPIs/store/controllers/UserController.java
+++ src/main/java/com/SpringBootRESTAPIs/store/controllers/UserController.java
@@ -3,21 +3,24 @@ package com.SpringBootRESTAPIs.store.controllers;
 import com.SpringBootRESTAPIs.store.entities.User;
 import com.SpringBootRESTAPIs.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
 @AllArgsConstructor
+@RestController
+@RequestMapping("/users") // this is the base path for all the endpoints in this controller, so we don't have to write /users in every endpoint
 public class UserController {
     private final UserRepository userRepository;
 
 //    @RequestMapping(path = "/users",method = RequestMethod.GET)
 //    @RequestMapping(path = "/users")
     // These above 2 lines are the same as bellow GetMapping
-    @GetMapping("/users")
+    @GetMapping // /users
     public Iterable<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}") // /users/{id}
+    public User getUser(@PathVariable("id") Long id){
+        return userRepository.findById(id).orElse(null);
     }
 }

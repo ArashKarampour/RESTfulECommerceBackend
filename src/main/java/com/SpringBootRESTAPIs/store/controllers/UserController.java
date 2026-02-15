e8964936b0e3e26adc80c93c49f873b7dce1a4bb
Arash Karampour <karampour.arash@yahoo.com>
2026-02-16 00:22:42 +0100

    using mapstruct library to map entities(a user) easier and during complile time

--- src/main/java/com/SpringBootRESTAPIs/store/controllers/UserController.java
+++ src/main/java/com/SpringBootRESTAPIs/store/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.SpringBootRESTAPIs.store.controllers;
 
 import com.SpringBootRESTAPIs.store.dtos.UserDto;
 import com.SpringBootRESTAPIs.store.entities.User;
+import com.SpringBootRESTAPIs.store.mappers.UserMapper;
 import com.SpringBootRESTAPIs.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,15 +14,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users") // this is the base path for all the endpoints in this controller, so we don't have to write /users in every endpoint
 public class UserController {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-//    @RequestMapping(path = "/users",method = RequestMethod.GET)
+    //    @RequestMapping(path = "/users",method = RequestMethod.GET)
 //    @RequestMapping(path = "/users")
     // These above 2 lines are the same as bellow GetMapping
     @GetMapping // /users
     public Iterable<UserDto> getAllUsers() { // we use UserDto instead of User to hide some unnecessary fields(security) and for better serialization/deserialization
         return userRepository.findAll() // userRepository interface should extend JpaRepository (for findAll) to return a List to map with stream to UserDto
                 .stream()
-                .map(user -> new UserDto(user.getId(), user.getName(), user.getEmail()))
+                .map(userMapper::toDto)
+//                .map(user -> new UserDto(user.getId(), user.getName(), user.getEmail()))
                 .toList();
     }
 
@@ -32,8 +35,8 @@ public class UserController {
 //            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             return ResponseEntity.notFound().build();
         }
-        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+//        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
 //        return new ResponseEntity<>(user, HttpStatus.OK);
-        return ResponseEntity.ok(userDto);
+        return ResponseEntity.ok(userMapper.toDto(user));
     }
 }
---
+++ src/main/java/com/SpringBootRESTAPIs/store/mappers/UserMapper.java
@@ -0,0 +1,10 @@
+package com.SpringBootRESTAPIs.store.mappers;
+
+import com.SpringBootRESTAPIs.store.dtos.UserDto;
+import com.SpringBootRESTAPIs.store.entities.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    UserDto toDto(User user); // this will map a user to a userdto using the mapstruct library. the name of the method is not important just the input type and output type are important.
+}

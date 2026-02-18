78af08a8136690a7ef14f5baaf400177cda1f6b0
Arash Karampour <karampour.arash@yahoo.com>
2026-02-18 15:29:05 +0100

    adding create , update and delete endpoints for ProductController

--- src/main/java/com/SpringBootRESTAPIs/store/controllers/ProductController.java
+++ src/main/java/com/SpringBootRESTAPIs/store/controllers/ProductController.java
@@ -3,13 +3,15 @@ package com.SpringBootRESTAPIs.store.controllers;
 import com.SpringBootRESTAPIs.store.dtos.ProductDto;
 import com.SpringBootRESTAPIs.store.entities.Product;
 import com.SpringBootRESTAPIs.store.mappers.ProductMapper;
+import com.SpringBootRESTAPIs.store.repositories.CategoryRepository;
 import com.SpringBootRESTAPIs.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
-import java.util.Objects;
 
 @AllArgsConstructor
 @RestController
@@ -18,6 +20,7 @@ public class ProductController {
 
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+    private final CategoryRepository categoryRepository;
 
     @GetMapping
     public Iterable<ProductDto> getAllProducts(@RequestParam(required = false, defaultValue = "0", name="categoryId") Byte categoryId){
@@ -46,5 +49,54 @@ public class ProductController {
         if(product == null)
             return ResponseEntity.notFound().build();
         return ResponseEntity.ok(productMapper.toDto(product));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(
+            @RequestBody ProductDto productDto,
+            UriComponentsBuilder uriBuilder
+    )
+    {
+        var category = categoryRepository.findById(productDto.getCategoryId()).orElse(null); // we need to fetch the category from the database to set it in the product entity before saving it,
+        if(category == null)                                                                       // because the product entity has a many-to-one relationship with the category entity, so we need to set the category in the product entity before saving it (because category_id must be set in the database for a product).
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+
+        var product = productMapper.toEntity(productDto);
+        product.setCategory(category);
+        var savedProductDto = productMapper.toDto(productRepository.save(product));
+        var uri = uriBuilder.path("/products/{id}").buildAndExpand(savedProductDto.getId()).toUri();
+        return ResponseEntity.created(uri).body(savedProductDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDto> updateProduct(
+            @PathVariable Long id,
+            @RequestBody ProductDto productDto
+    )
+    {
+        var product = productRepository.findById(id).orElse(null);
+        if (product == null)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        var category = categoryRepository.findById(productDto.getCategoryId()).orElse(null); // we need to fetch the category from the database to set it in the product entity before saving it,
+        if(category == null)                                                                       // because the product entity has a many-to-one relationship with the category entity, so we need to set the category in the product entity before saving it (because category_id must be set in the database for a product).
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+
+        product.setCategory(category);
+        productMapper.update(productDto, product); // in this update implementation of mapstruct, you can find under target/annotations/ProductMapperImpl.java the category is not updated. that's why we need to set the category before calling the update method, because the update method will not update the category field in the product entity, so we need to set it before calling the update method, and then we will update the other fields of the product entity with the values from the productDto.
+        productRepository.save(product);
+        return ResponseEntity.status(HttpStatus.OK).body(productMapper.toDto(product));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct (
+            @PathVariable Long id
+    )
+    {
+        var product = productRepository.findById(id).orElse(null);
+        if (product == null)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        productRepository.delete(product);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
--- src/main/java/com/SpringBootRESTAPIs/store/mappers/ProductMapper.java
+++ src/main/java/com/SpringBootRESTAPIs/store/mappers/ProductMapper.java
@@ -4,10 +4,14 @@ import com.SpringBootRESTAPIs.store.dtos.ProductDto;
 import com.SpringBootRESTAPIs.store.entities.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
     @Mapping(source = "category.id", target = "categoryId") // this will map id of the category field from Product to categoryId of ProductDto
     ProductDto toDto(Product product);
+    Product toEntity(ProductDto productDto);
+    @Mapping(target = "id", ignore = true) // we need to ignore the id field because we don't want to update it, we want to keep the same id of the product, and we will update only the other fields, so we need to ignore the id field in the mapping.
+    void update(ProductDto productDto, @MappingTarget Product product);
 }

3b825e75e0a6d8bd1bca9173aaf63d5a4fe944cc
Arash Karampour <karampour.arash@yahoo.com>
2026-02-27 00:42:56 +0100

    adding authentication manger using UserDetailsService and authenticate users using authetication manager

--- src/main/java/com/SpringBootRESTAPIs/store/config/SecurityConfig.java
+++ src/main/java/com/SpringBootRESTAPIs/store/config/SecurityConfig.java
@@ -1,24 +1,46 @@
 package com.SpringBootRESTAPIs.store.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+    private UserDetailsService userDetailsService;
     // adding methods to hash passwords and configure security filter chain for stateless sessions (token-based authentication)
     @Bean
     public PasswordEncoder passwordEncoder() { // this method will be used to hash passwords before saving them to the database, and to compare the hashed password with the plain text password when logging in, we use BCryptPasswordEncoder which is a strong hashing algorithm that is widely used in the industry for password hashing, it also adds a salt to the password before hashing it to protect against rainbow table attacks, and it is also computationally expensive which makes it resistant to brute force attacks.
         return new BCryptPasswordEncoder();
     } // how this works under the hood by dependency injection system of the Spring framework: https://chatgpt.com/share/69a060dc-9168-8002-9dcb-27be9f5d62c5
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        var provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
--- src/main/java/com/SpringBootRESTAPIs/store/controllers/AuthController.java
+++ src/main/java/com/SpringBootRESTAPIs/store/controllers/AuthController.java
@@ -1,37 +1,44 @@
 package com.SpringBootRESTAPIs.store.controllers;
 
 import com.SpringBootRESTAPIs.store.dtos.LoginUserRequest;
-import com.SpringBootRESTAPIs.store.mappers.UserMapper;
-import com.SpringBootRESTAPIs.store.repositories.UserRepository;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("/auth") // this is the base path for all the endpoints in this controller, so we don't have to write /auth in every endpoint
+@RequestMapping("/auth")
 public class AuthController {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final UserMapper userMapper;
+//    private final UserRepository userRepository;
+//    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(
             @Valid @RequestBody LoginUserRequest request
     ) {
-        var user = userRepository.findByEmail(request.getEmail()).orElse(null);
-        if (user == null || !passwordEncoder.matches(request.getPassword(), user.getPassword())) { // this is a simple authentication check, in a real application we should use a more secure authentication mechanism, such as JWT tokens or sessions, to authenticate the user and to manage the user's authentication state.
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", "Invalid email or password!")); // this will return a response with status code 401 and a body with the error message, which is the standard response for an unauthorized request.
-        }
+//        var user = userRepository.findByEmail(request.getEmail()).orElse(null);
+//        if (user == null || !passwordEncoder.matches(request.getPassword(), user.getPassword())) { // this is a simple authentication check, in a real application we should use a more secure authentication mechanism, such as JWT tokens or sessions, to authenticate the user and to manage the user's authentication state.
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", "Invalid email or password!")); // this will return a response with status code 401 and a body with the error message, which is the standard response for an unauthorized request.
+//        }
+        // handling authentication using the authentication manager provided by Spring Security, which will handle the authentication process and throw an exception if the credentials are invalid, we will handle this exception using an exception handler method annotated with @ExceptionHandler to return a response with status code 401 and no body, which is the standard response for an unauthorized request.
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
         return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler(BadCredentialsException.class) // this annotation is used to handle the exception thrown by the authentication manager when the credentials are invalid, and to return a response with status code 401 and no body, which is the standard response for an unauthorized request.(instead of returning 403)
+    public ResponseEntity<Void> handleBadCredentials() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }

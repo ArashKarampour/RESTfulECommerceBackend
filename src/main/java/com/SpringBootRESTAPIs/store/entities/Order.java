417d9d6e9b409803956cb769d5fb58e8eacbfeb3
Arash Karampour <karampour.arash@yahoo.com>
2026-03-23 19:33:15 +0100

    adding the order and order itesm entities

---
+++ src/main/java/com/SpringBootRESTAPIs/store/entities/Order.java
@@ -0,0 +1,43 @@
+package com.SpringBootRESTAPIs.store.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private User customer;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @Column(name = "created_at", insertable = false, updatable = false) // we will set the createdAt value in the database using a default value, so we don't want to insert or update it from our application using hibernate.
+    private LocalDateTime createdAt;
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+
+    @OneToMany(mappedBy = "order")
+    private Set<OrderItem> items = new LinkedHashSet<>();
+
+
+}
